2768cffd40be0a8212fe8d93c8b7c555f57fd6a8
ygn_js <78275383+ygnJavascript@users.noreply.github.com>
2023-02-24 15:02:01 +0300

    Update myrules.js

--- myrules.js
+++ myrules.js
@@ -1,6 +1,3 @@
 window.listenRules=[];
-window.rule={}
-window.rule.f=(x)=>{return document.querySelector(x)}
-window.rule.fa=(x)=>{return document.querySelectorAll(x)}
-window.rule.new=(attr,attrName,code,type="addedNodes")=>{let nextObjmr={};nextObjmr.attr=attr;nextObjmr.attrName=attrName;nextObjmr.code=code;nextObjmr.type=type;window.listenRules.push(nextObjmr);}
+function newRule(attr,attrName,code,type="addedNodes"){let nextObj={};nextObj.attr=attr;nextObj.attrName=attrName;nextObj.code=code;nextObj.type=type;window.listenRules.push(nextObj);}
 let targetNodemr=document.querySelector("#content"),observer=new MutationObserver(mutations=>{mutations.forEach(mutation=>{window.listenRules.forEach(rule=>{mutation.type==='childList'&&mutation[rule.type].length>0?rule&&mutation[rule.type][0].getAttribute(rule.attr)==rule.attrName?rule.code(mutation[rule.type][0]):0:0})});});observer.observe(targetNodemr,{childList:true,subtree:true});
